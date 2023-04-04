20a4ec9ca4db9e118d9b09f297b7b39e1a959ede
Borisaye <Boris_aroche@hotmail.com>
2023-04-04 11:40:17 -0400

    .

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+function saturdayFun(satActivity = "roller-skate"){
+
+    return `This Saturday, I want to ${satActivity}!`
+
+
+}
+
+function mondayWork(monWork = "go to the office"){
+
+    return `This Monday, I will ${monWork}.`
+
+}
+
+function wrapAdjective(charWrap = "*"){
+    const msgAdj = function(AdjDesc = "Special"){
+
+    return `You are ${charWrap}${AdjDesc}${charWrap}!`;
+
+
+    }
+    return msgAdj;
+}
